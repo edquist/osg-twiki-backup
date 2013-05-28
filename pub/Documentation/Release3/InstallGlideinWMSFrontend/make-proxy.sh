ce4bb560ce654742c7eb86fe9c36b1cb506d1507
MarcoMambelli <MarcoMambelli>
2013-05-28 19:06:02 +0000

    r1.2 pub/Documentation/Release3/InstallGlideinWMSFrontend/make-proxy.sh
    Example of make-proxy script

--- pub/Documentation/Release3/InstallGlideinWMSFrontend/make-proxy.sh
+++ pub/Documentation/Release3/InstallGlideinWMSFrontend/make-proxy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Script to refresh the a proxy with VO attributes - Marco Mambelli marco@hep.uchicago.edu
+# make-proxy [--no-voms-proxy]
+#  --no-voms-proxy creates only a grid proxy from the certificates (2a below), no VOMS proxy command is run
+# TO RUN THE SCRIPT
 # 0. Save this script and make sure that it is executable
 # 1. Personalize the script by filling correctly the variables below
 # 2a. If you provide CERT_FILE and KEY_FILE these will be used to generate the base-proxy 
@@ -15,6 +18,8 @@
 #  Remember that the proxy needs to be renewed once a year  (you will receive an email)
 # 3. Add a Cron entry like
 #    10 * * * * /full_path_to_script_make-proxy
+
+
 
 # A cron entry like
 # 10 * * * * . /share/wlcg-client/setup.sh; voms-proxy-init -valid 48:0 -voms voms_option -key $HOME/.globus/base_proxy -cert $HOME/.globus/base_proxy -out my_proxy_with_atlas_voms_attribute >/dev/null 2>&1 
@@ -37,13 +42,21 @@ OWNER_EMAIL="marco@hep.uchicago.edu"
 CC_EMAIL=""
 # Proxy description used in the email sent when the proxy is missing, expired os about to expire
 PROXY_DESCRIPTION="VO Fronted on ui-gwms"
-# The VO option for the proxy, usually "voname:/voname", e.g. "osg:/osg"
+# The VO option for the proxy, usually "voname:/voname"
 VOMS_OPTION=""
-# Some VOMS servers, like the OSG one fail the AC signature verification
-# If voms-proxy commands regularly fail with "Cannot verify AC signature!"
-# Then uncomment the line with the special option dont-verify-ac
-VOMS_NOCHECK=""
-#VOMS_NOCHECK="-dont-verify-ac"
+VOMS_EXTRA_OPTION="-dont-verify-ac"
+
+##### PARAMETERS
+VOMS_ACTION=
+PROXY_TIME="500:0"
+PROXY_VERTIME="150:0"
+VOMS_PROXY_TIME="64:0"
+
+if [ "x$1" == "x--no-voms-proxy" ]; then
+   VOMS_ACTION="novoms"
+   PROXY_TIME="50:0"
+   PROXY_VERTIME="24:0"
+fi
 
 ##### SCRIPT
 EMAIL_MESSAGE="
@@ -75,24 +88,33 @@ NEW_CERT_MESSAGE=""
 if [[ ! -z $CERT_FILE &&  ! -z $KEY_FILE && -f $CERT_FILE && -f $KEY_FILE ]]
 then
    NEW_CERT_MESSAGE="Th proxy was generated from the certificate $CERT_FILE and key $KEY_FILE."
-   grid-proxy-init -valid 500:0 -key $KEY_FILE  -cert $CERT_FILE -out $IN_NAME >/dev/null 2>&1
+   grid-proxy-init -valid $PROXY_TIME -key $KEY_FILE  -cert $CERT_FILE -out $IN_NAME >/dev/null 2>&1
    if [ $? -ne 0 ]; then
-      echo "The generation of the proxy file $IN_NAME for $PROXY_DESCRIPTION failed.\n Please login on `hostname` and check the certificate $CERT_FILE and key $KEY_FILE. As $USER run manually:\ngrid-proxy-init -valid 500:0 -key $KEY_FILE  -cert $CERT_FILE -out $IN_NAME" | mail -s "$PROXY_DESCRIPTION proxy error due to certificates." $OWNER_EMAIL $CC_EMAIL_OPTION
+      echo -e "The generation of the proxy file $IN_NAME for $PROXY_DESCRIPTION failed.\n Please login on `hostname` and check the certificate $CERT_FILE and key $KEY_FILE. As $USER run manually:\ngrid-proxy-init -valid $PROXY_TIME -key $KEY_FILE  -cert $CERT_FILE -out $IN_NAME" | mail -s "$PROXY_DESCRIPTION proxy error due to certificates." $OWNER_EMAIL $CC_EMAIL_OPTION
       exit 0
    fi
 fi
 
 if [ -f $IN_NAME ]; 
 then
-   grid-proxy-info -exists -valid 150:0 -file $IN_NAME
+   grid-proxy-info -exists -valid $PROXY_VERTIME -file $IN_NAME
    if [ $? -ne 0 ]; then
       echo "The proxy file $IN_NAME for $PROXY_DESCRIPTION has or will expire soon. $NEW_CERT_MESSAGE $EMAIL_MESSAGE" | mail -s "$PROXY_DESCRIPTION proxy has or will expire soon." $OWNER_EMAIL -c $CC_EMAIL
    fi
 else
    echo "The proxy file $IN_NAME for $PROXY_DESCRIPTION is missing. $NEW_CERT_MESSAGE $EMAIL_MESSAGE" | mail -s "Missing $PROXY_DESCRIPTION proxy" $OWNER_EMAIL $CC_EMAIL_OPTION
 fi
-voms-proxy-init -valid 48:0 -key $IN_NAME -cert $IN_NAME $VOMS_OPTION_STRING -out $OUT_NAME $VOMS_NOCHECK >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-   echo "The proxy file $IN_NAME seems OK but voms-proxy-init is failing. $NEW_CERT_MESSAGE\nPlease login on `hostname` and as $USER run manually:\nvoms-proxy-init -valid 48:0 -key $IN_NAME -cert $IN_NAME $VOMS_OPTION_STRING -out $OUT_NAME $VOMS_NOCHECK" | mail -s "$PROXY_DESCRIPTION proxy renewal broken." $OWNER_EMAIL $CC_EMAIL_OPTION
+
+# Exit if no VOMS proxy is required
+if [ "x$VOMS_ACTION" == "xnovoms" ]; then
+   cp $IN_NAME $OUT_NAME
+   chmod 600 $OUT_NAME
+   exit 0
 fi
+
+voms-proxy-init -valid $VOMS_PROXY_TIME -key $IN_NAME -cert $IN_NAME $VOMS_OPTION_STRING -out $OUT_NAME $VOMS_EXTRA_OPTION >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+   echo -e "The proxy file $IN_NAME seems OK but voms-proxy-init is failing. $NEW_CERT_MESSAGE\nPlease login on `hostname` and as $USER run manually:\nvoms-proxy-init -valid $VOMS_PROXY_TIME -key $IN_NAME -cert $IN_NAME $VOMS_OPTION_STRING -out $OUT_NAME $VOMS_EXTRA_OPTION" | mail -s "$PROXY_DESCRIPTION proxy renewal broken." $OWNER_EMAIL $CC_EMAIL_OPTION
+fi
+
 
